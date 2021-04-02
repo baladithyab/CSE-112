7677aaa2c53293de9afac3154a9454f565262423
Baladithya Balamurugan <bbaladithya@gmail.com>
2021-04-01 21:17:15 -0700

    PAIN

--- asg1/mbir.scm
+++ asg1/mbir.scm
@@ -3,3 +3,11 @@
 ;; File: mbir.scm
 
 
+
+;; (file->lines "00-hello-world.mbir")
+
+(display (read-string 200 (open-input-file "00-hello-world.mbir")))
+(display "hello world")
+(newline)
+(display ())
+
