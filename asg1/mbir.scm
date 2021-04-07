621214ddbccd30bb33eb625796d4c11a85609903
Baladithya Balamurugan <bbaladithya@gmail.com>
2021-04-06 19:05:14 -0700

    fileinput and car/cdr testing

--- asg1/mbir.scm
+++ asg1/mbir.scm
@@ -6,8 +6,29 @@
 
 ;; (file->lines "00-hello-world.mbir")
 
-(display (read-string 200 (open-input-file "00-hello-world.mbir")))
+
+(require (planet williams/describe/describe))
+(define *ARG-LIST* (vector->list (current-command-line-arguments)))
+
+
+; (display (read-string 200 (open-input-file "00-hello-world.mbir")))
 (display "hello world")
 (newline)
-(display ())
+(display *ARG-LIST*)
+(newline)
+(define source (open-input-file (car *ARG-LIST*)))
+(define sourcey (read source))
+(newline)
+(display (describe sourcey))
+(newline)
+;; https://stackoverflow.com/questions/32212874/scheme-how-to-print-a-list-without-parenthesis
+(define (displist lst)
+  (let loop ((lst lst))
+    (when (pair? lst)
+      (display (car lst))
+      (newline)
+      (loop (cdr lst))))
+  (newline))
+(displist sourcey)
+
 
