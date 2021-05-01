91096da41da2b9025206dfa06b818c1b0dc5a683
Baladithya Balamurugan <bbaladithya@gmail.com>
2021-05-01 15:23:44 -0700

    fixed dumb error

--- asg2/code/interp.ml
+++ asg2/code/interp.ml
@@ -1,5 +1,4 @@
 (* $Id: interp.ml,v 1.19 2021-04-20 20:19:30-07 - - $ *)
-
 open Absyn
 
 let want_dump = ref false
@@ -11,25 +10,20 @@ let rec eval_expr (expr : Absyn.expr) : float = match expr with
     | Number number -> number
     | Memref memref -> eval_memref memref
     | Unary (oper, expr) -> eval_STUB "eval_expr Unary"
-        (*NEED TO IMPLEMENT*)
     | Binary (oper, expr1, expr2) -> eval_STUB "eval_expr Binary"
-        (*NEED TO IMPLEMENT*)
 
-(*NEED TO IMPELEMENT*)
 and eval_memref (memref : Absyn.memref) : float = match memref with
     | Arrayref (ident, expr) -> eval_STUB "eval_memref Arrayref"
-        (*NEED TO IMPLEMENT*)
     | Variable ident -> try Hashtbl.find Tables.variable_table ident
                         with Not_found -> 0.0
-        (*NEED TO IMPLEMENT*)
 
 and eval_STUB reason = (
     print_string ("(" ^ reason ^ ")");
     nan)
 
-let print_not_found (func) =
-    Print.printf "Error in " func ", label not found."
-    exit(1)
+let print_not_found func =
+    (Printf.printf "Error in %s, label not found." func;
+    exit(1);)
 
 let rec interpret (program : Absyn.program) = match program with
     | [] -> ()
@@ -37,8 +31,6 @@ let rec interpret (program : Absyn.program) = match program with
        | _, _, None -> interpret continue
        | _, _, Some stmt -> (interp_stmt stmt continue)
 
-
-(*NEED TO IMPLEMENT*)
 and interp_stmt (stmt : Absyn.stmt) (continue : Absyn.program) =
     match stmt with
     | Dim (ident, expr) -> interp_STUB "Dim (ident, expr)" continue
@@ -47,27 +39,6 @@ and interp_stmt (stmt : Absyn.stmt) (continue : Absyn.program) =
     | If (expr, label) -> interp_STUB "If (expr, label)" continue
     | Print print_list -> interp_print print_list continue
     | Input memref_list -> interp_input memref_list continue
-
-(*NEED TO IMPLEMENT*)
-and interp_LET (memref, expr) (continue : Absyn.program) =
-    
-
-(*NEED TO IMPLEMENT*)
-and interp_GOTO (label) (continue : Absyn.program) = 
-(*try Hashtbl.find Tables.variable_table ident
-                        with Not_found -> 0.0*)
-    try
-        let flag = Hashtbl.find Tables.label_table label in interpret flag
-    with
-        Not_found => print_not_found "GOTO"
-
-(*NEED TO IMPLEMENT*)
-and interp_IF (expr, label) (continue : Absyn.program) =
-
-
-(*NEED TO IMPLEMENT*)
-and interp_DIM (ident, expr) (continue : Absyn.program) =
-
 
 and interp_print (print_list : Absyn.printable list)
                  (continue : Absyn.program) =
@@ -80,6 +51,27 @@ and interp_print (print_list : Absyn.printable list)
     in (List.iter print_item print_list; print_newline ());
     interpret continue
 
+(* NEED TO IMPLEMENT *)
+and interp_LET (memref, expr) (continue : Absyn.program) = (print_not_found "LET")
+    
+
+(*NEED TO IMPLEMENT*)
+and interp_GOTO (label) (continue : Absyn.program) = 
+(*try Hashtbl.find Tables.variable_table ident
+                        with Not_found -> 0.0*)
+    try
+        let flag = Hashtbl.find Tables.label_table label in interpret flag
+    with
+        Not_found -> print_not_found "GOTO"
+
+(*NEED TO IMPLEMENT*)
+and interp_IF (expr, label) (continue : Absyn.program) = (print_not_found "IF")
+
+
+(*NEED TO IMPLEMENT*)
+and interp_DIM (ident, expr) (continue : Absyn.program) = (print_not_found "DIM")
+
+
 and interp_input (memref_list : Absyn.memref list)
                  (continue : Absyn.program)  =
     let input_number memref =
